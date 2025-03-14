3dc5ea794c2c5279be50d9db47606cfb413b2dc5
iamKhang <iamhoangkhang@icloud.com>
2025-03-14 14:14:28 +0700

    Add user retrieval endpoints to UserController and implement corresponding service methods for fetching user details and basic info, including error handling for non-existent users.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,7 +4,48 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
   async getAllUsers() {
     return this.prisma.user.findMany();
+  }
+
+  async getUserById(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        phoneNumber: true,
+        createdAt: true,
+        updatedAt: true,
+        userInfo: {
+          select: {
+            fullName: true,
+            dateOfBirth: true,
+            gender: true,
+            bio: true,
+            profilePictureUrl: true,
+            statusMessage: true,
+            lastSeen: true,
+            coverImgUrl: true,
+          },
+        },
+      },
+    });
+  }
+
+  async getUserBasicInfo(id: string) {
+    return this.prisma.userInfo.findUnique({
+      where: { id },
+      select: {
+        fullName: true,
+        dateOfBirth: true,
+        gender: true,
+        bio: true,
+        profilePictureUrl: true,
+        statusMessage: true,
+        coverImgUrl: true,
+      },
+    });
   }
 }
